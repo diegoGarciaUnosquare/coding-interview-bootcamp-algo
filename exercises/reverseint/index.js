31a3b1f295135af81a18c1fb6d65baaf57c51486
Diego Garcia <diego.garcia@unosquare.com>
2026-05-05 17:04:08 -0600

    finished section 5

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    // My solution (almost complete)
+    // const intStrigified = n.toString();
+    // const charArray = intStrigified.split('');
+    // const reversedString = charArray.reduce((prev, current) => current + prev, '');
+    // return parseInt(reversedString);
 
-module.exports = reverseInt;
+    // Solution 1
+    // const reversed = n.toString().split('').reverse().join('');
+    
+    // if (n < 0 ) {
+    //     return parseInt(reversed) * -1;
+    // }
+    // return parseInt(reversed);
+
+    // Solution 2
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n); 
+
+
+}
+
+module.exports = reverseInt; 
