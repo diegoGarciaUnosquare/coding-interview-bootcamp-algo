e9c0990544c6632d76f9dce5c2bb3d1975a5f822
Diego Garcia <diego.garcia@unosquare.com>
2026-05-05 12:47:14 -0600

    completed section 3

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,30 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // Solution 1
+    // return str.split('').reverse().join('');
+
+    // Solution 2
+    // let reversed = '';
+    // for (let character of str) {
+    //     reversed = character + reversed;
+    // }
+    // return reversed;
+
+    // Solution 3 -- More Complicated solution
+    return str.split('').reduce((reversed, character) => character + reversed, '');
+     
+    // My Solution
+    // let reversed = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     reversed += str[i];
+    // }
+    // return reversed;
+}
 
 module.exports = reverse;
